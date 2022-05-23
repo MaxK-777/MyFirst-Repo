afda8137cf844a3e31301db9049a7d25099f2c61
Max <kunstmax747@gmail.com>
2022-05-23 19:46:12 +0200

    adding a file

---
+++ SendToJeren.R
@@ -0,0 +1,10 @@
+# Thgings to be transferred
+
+#1. https://github.com/MaxK-777/MyFirst-Repo.git
+
+# Git: https://git-scm.com/downloads
+
+# Super simple
+# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+
+# Also very simple: https://jennybc.github.io/2014-05-12-ubc/ubc-r/session03_git.html
